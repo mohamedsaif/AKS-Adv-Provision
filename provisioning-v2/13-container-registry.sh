fc3ec39f9989a9ece410aa82e4d394acaaa1eea8
Mohamed Saif <mohamed.saif@outlook.com>
2020-05-28 16:33:55 +0400

    Minor fix in ACR script

--- provisioning-v2/13-container-registry.sh
+++ provisioning-v2/13-container-registry.sh
@@ -8,7 +8,7 @@ az acr create \
     -g $RG_SHARED \
     -n $CONTAINER_REGISTRY_NAME \
     --sku Basic \
-    --workspace $SHARED_WORKSPACE_NAME
+    --workspace $SHARED_WORKSPACE_NAME \
     --tags $TAG_ENV_DEV $TAG_PROJ_SHARED $TAG_DEPT_IT $TAG_STATUS_EXP
 
 # (PREVIEW) Private Link Support
